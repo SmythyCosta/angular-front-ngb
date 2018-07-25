143826a59036fb84192f8317a3bbbd5be4798cba
Smythy <smythy.costa@hotmail.com>
2018-07-25 17:37:03 -0300

    criando Category Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 //Modulos
 import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
+import { CategoryModule } from './category/category.module';
+
 
 //Components
 import { AppComponent } from './app.component';
-import { CategoryComponent } from './category/category.component';
 import { SubCategoryComponent } from './sub-category/sub-category.component';
+
 
 
 @NgModule({
     declarations: [
-        AppComponent, CategoryComponent, SubCategoryComponent
+        AppComponent, SubCategoryComponent
     ],
     imports: [
-        BrowserModule, AppBootstrapModule
+        BrowserModule, AppBootstrapModule, CategoryModule
     ],
     providers: [],
     bootstrap: [AppComponent]
