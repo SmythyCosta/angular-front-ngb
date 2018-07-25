46fffff08f1249aef64fc6912271b8f6a994f2a4
Smythy <smythy.costa@hotmail.com>
2018-07-25 17:23:39 -0300

    Criando o componente SubCategory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
 //Components
 import { AppComponent } from './app.component';
 import { CategoryComponent } from './category/category.component';
+import { SubCategoryComponent } from './sub-category/sub-category.component';
 
 
 @NgModule({
     declarations: [
-        AppComponent, CategoryComponent
+        AppComponent, CategoryComponent, SubCategoryComponent
     ],
     imports: [
         BrowserModule, AppBootstrapModule
