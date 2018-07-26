304d763c759b1069119f9969b299a0c4d426fc68
Smythy <smythy.costa@hotmail.com>
2018-07-26 15:14:31 -0300

    sub componentes de Category => CatoryList e CategoryForm

--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryComponent } from './category.component';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryFormComponent } from './category-form/category-form.component';
 
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [CategoryComponent],
+  declarations: [CategoryComponent, CategoryListComponent, CategoryFormComponent],
   exports: [CategoryComponent]
 })
 export class CategoryModule { }
