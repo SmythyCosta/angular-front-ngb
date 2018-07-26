58dddbc3076082f1ee258fb4d6a3311678d4ed5a
Smythy <smythy.costa@hotmail.com>
2018-07-26 17:23:26 -0300

    importando as rotas filhas ao CategoryModule

--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
+import { CategoryRoutingModule } from './category-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    CategoryRoutingModule
   ],
   declarations: [CategoryListComponent, CategoryFormComponent],
   exports: [CategoryListComponent, CategoryFormComponent]
