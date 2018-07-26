ef8e737075279356be0dc2c4ee89e40f582dbf08
Smythy <smythy.costa@hotmail.com>
2018-07-26 10:27:09 -0300

    import o modulo de Rotas para p modulo principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 
-
 //Modulos
 import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
+import { AppRoutingModule } from './app.routes';
 import { CategoryModule } from './category/category.module';
 import { SubCategoryModule } from './sub-category/sub-category.module';
-
 
 //Components
 import { AppComponent } from './app.component';
@@ -18,7 +17,7 @@ import { AppComponent } from './app.component';
         AppComponent
     ],
     imports: [
-        BrowserModule, AppBootstrapModule, HttpModule, CategoryModule, SubCategoryModule
+        BrowserModule, AppBootstrapModule, HttpModule, CategoryModule, SubCategoryModule, AppRoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
