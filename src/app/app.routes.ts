1c04bcf624d7c3757d1020840f6504de00d821c6
Smythy <smythy.costa@hotmail.com>
2018-07-26 11:56:06 -0300

    adicionando dashboard a rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { CategoryComponent } from './category/category.component';
 import { SubCategoryComponent } from './sub-category/sub-category.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 const routes: Routes = [
-    { path: '', redirectTo: '/category', pathMatch: 'full' },
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: 'dashboard',        component: DashboardComponent },
     { path: 'category',         component: CategoryComponent },
     { path: 'sub-category',     component: SubCategoryComponent },
-    { path: '**',               component: SubCategoryComponent }
+    { path: '**',               component: DashboardComponent }
 ];
 
 
