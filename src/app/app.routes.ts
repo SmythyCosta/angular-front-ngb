118fb30802d72ce187f46ae8c198dea21137867c
Smythy <smythy.costa@hotmail.com>
2018-07-26 15:45:22 -0300

    import os componentes no arquivo de rotas Pai

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { CategoryComponent } from './category/category.component';
 import { SubCategoryComponent } from './sub-category/sub-category.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CategoryListComponent } from './category/category-list/category-list.component';
 
 
 const routes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     { path: 'dashboard',        component: DashboardComponent },
-    { path: 'category',         component: CategoryComponent },
+    { path: 'category',         component: CategoryListComponent },
     { path: 'sub-category',     component: SubCategoryComponent },
     { path: '**',               component: DashboardComponent }
 ];
