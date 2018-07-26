f5ac7fe64f7bcf10a4c281dace8c9aad5ebc4fcc
Smythy <smythy.costa@hotmail.com>
2018-07-26 12:00:14 -0300

    estrutura de navegação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ import 'rxjs/Rx';
 })
 export class AppComponent {
 
+    title = 'Dashboard - FrontEnd Angular Ecommerce';
     categoryList:Object[] = [];
 
     constructor(http:Http){
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,8 +7,6 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
-    title = 'Dashboard - FrontEnd Angular Ecommerce';
-
     constructor() { }
 
 }
