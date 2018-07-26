04030fc05886197cd58331aa6f6d3f912710be0e
Smythy <smythy.costa@hotmail.com>
2018-07-26 10:23:25 -0300

    arquivo app.routes finalizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-/*
-arquivos de Rotas 
-*/
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { CategoryComponent } from './category/category.component';
+import { SubCategoryComponent } from './sub-category/sub-category.component';
+
+
+const routes: Routes = [
+    { path: '', redirectTo: '/category', pathMatch: 'full' },
+    { path: 'category',         component: CategoryComponent },
+    { path: 'sub-category',     component: SubCategoryComponent }
+];
+
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
