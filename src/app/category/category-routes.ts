26eb85294ccf10d22eafdde9424229c61acf1af5
Smythy Costa <smythy.costa@hotmail.com>
2018-07-27 20:51:55 -0300

    CategoryRoutes

---
+++ src/app/category/category-routes.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryFormComponent } from './category-form/category-form.component';
+
+
+const categoryRoutes: Routes = [
+    { path: 'category',        component: CategoryListComponent },
+    { path: 'category/form',   component: CategoryFormComponent }
+];
+
+
+@NgModule({
+    imports: [RouterModule.forChild(categoryRoutes)],
+    exports: [RouterModule]
+})
+export class CategoryRoutes { }
