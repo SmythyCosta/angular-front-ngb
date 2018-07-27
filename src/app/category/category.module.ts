ee181426f56837e688a9011040d23753f3d578c1
Smythy Costa <smythy.costa@hotmail.com>
2018-07-27 20:53:05 -0300

    CategoryModule

--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule }     from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule }  from '@angular/forms';
 
-import { CategoryListComponent } from './category-list/category-list.component';
-import { CategoryFormComponent } from './category-form/category-form.component';
+import { CategoryListComponent }    from './category-list/category-list.component';
+import { CategoryFormComponent }    from './category-form/category-form.component';
+import { CategoryRoutes }           from './category-routes';
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
+        CategoryRoutes
     ],
     declarations: [CategoryListComponent, CategoryFormComponent],
     exports: [CategoryListComponent, CategoryFormComponent]
