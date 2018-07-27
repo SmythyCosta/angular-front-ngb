e9b405d8441b3c55fc7ca3cacb99e56b142dd27e
Smythy Costa <smythy.costa@hotmail.com>
2018-07-26 21:01:12 -0300

    mudanças em CategoryModule

--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule }    from '@angular/forms';
+
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
 import { CategoryRoutingModule } from './category-routing.module';
@@ -7,9 +9,10 @@ import { CategoryRoutingModule } from './category-routing.module';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     CategoryRoutingModule
   ],
-  declarations: [CategoryListComponent, CategoryFormComponent],
+  //declarations: [CategoryListComponent, CategoryFormComponent],
   exports: [CategoryListComponent, CategoryFormComponent]
 })
 export class CategoryModule { }
