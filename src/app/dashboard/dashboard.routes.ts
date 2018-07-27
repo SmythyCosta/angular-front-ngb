917b6556528bc7b2f34366396e764571d4d970d7
Smythy Costa <smythy.costa@hotmail.com>
2018-07-27 20:45:42 -0300

    DasboardRoutes - atribuindo rota ao componente

--- src/app/dashboard/dashboard.routes.ts
+++ src/app/dashboard/dashboard.routes.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
 
 
 const dashboardRoutes: Routes = [
-    //{ path: 'dashboard',        redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: 'dashboard',        component: DashboardComponent },
 ];
 
 
