6f2cd738ddb012ca1866cb706a26655e4b5d78f9
Smythy Costa <smythy.costa@hotmail.com>
2018-07-27 20:41:38 -0300

    DashboardModule

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
+import { DasboardRoutes } from './dashboard.routes';
 
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [DashboardComponent],
-  exports: [DashboardComponent]
+    imports: [
+        CommonModule,
+        DasboardRoutes
+    ],
+    declarations: [DashboardComponent],
+    exports: [DashboardComponent]
 })
 export class DashboardModule { }
