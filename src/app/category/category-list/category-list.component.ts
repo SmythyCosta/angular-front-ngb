b1cfe3a0daada6f21867e4067b866e513a65f3be
Smythy Costa <smythy.costa@hotmail.com>
2018-07-29 08:26:20 -0300

    CategoryListComponent => preparando para chamar o service

--- src/app/category/category-list/category-list.component.ts
+++ src/app/category/category-list/category-list.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Http } from '@angular/http';
 
 @Component({
-  selector: 'app-category-list',
-  templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.css']
+    selector: 'app-category-list',
+    templateUrl: './category-list.component.html',
+    styleUrls: ['./category-list.component.css']
 })
 export class CategoryListComponent implements OnInit {
-  
-  constructor() { }
 
-  ngOnInit() {}
+    constructor(
+        public router: Router,
+        private http:Http,
+        //private dataService:CategoryService,
+    ) { }
+
+    ngOnInit() { }
 
 }
