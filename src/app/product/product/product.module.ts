096d1c1d898a47b7e182934e05a8d8a6be9ad943
Smythy <smythy.costa@hotmail.com>
2018-07-30 12:28:11 -0300

    importando o index de services

--- src/app/product/product/product.module.ts
+++ src/app/product/product/product.module.ts
@@ -1,11 +1,12 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {ReactiveFormsModule} from '@angular/forms';
+import { CommonModule }                 from '@angular/common';
+import { ReactiveFormsModule }          from '@angular/forms';
 //import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 //import { ProductRoutingModule } from './product-routing.module';
 import { ProductComponent } from './product.component';
-//import { ProductService } from '../_services/index';
+import { ProductService } from '../../_services/index';
+
 //import { DataTablesModule } from 'angular-datatables';
 
 @NgModule({
