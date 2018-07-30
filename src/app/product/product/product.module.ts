42c07eae6dfe0c8d84e61f8e69507c67202d7390
Smythy <smythy.costa@hotmail.com>
2018-07-30 15:50:56 -0300

    instalado o dataTables

--- src/app/product/product/product.module.ts
+++ src/app/product/product/product.module.ts
@@ -3,20 +3,18 @@ import { CommonModule }                 from '@angular/common';
 import { ReactiveFormsModule }          from '@angular/forms';
 import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-//import { ProductRoutingModule } from './product-routing.module';
 import { ProductComponent } from './product.component';
 import { ProductService } from '../../_services/index';
 import { ProductRoutes } from './product.routes';
 
-//import { DataTablesModule } from 'angular-datatables';
+import { DataTablesModule } from 'angular-datatables';
 
 @NgModule({
     imports: [
         CommonModule,
         NgbModule,
-        //ProductRoutingModule,
         ReactiveFormsModule,
-        //DataTablesModule
+        DataTablesModule,
         ProductRoutes
     ],
     declarations: [
