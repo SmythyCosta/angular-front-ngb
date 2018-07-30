33d9724ac1a9ba6d1b0d74c322e0165e108dabf1
Smythy <smythy.costa@hotmail.com>
2018-07-30 10:27:16 -0300

    variavel para o PATH das api's

--- src/app/_services/category.service.ts
+++ src/app/_services/category.service.ts
@@ -4,26 +4,28 @@ import { Http, Headers, RequestOptions, Response } from '@angular/http';
 @Injectable()
 export class CategoryService {
 
+    private pathApi: String = 'http://localhost:8000';
+
     constructor(private http: Http) { }
 
     save(data) {
-        return this.http.post('http://localhost:8000/api/category-save', data);
+        return this.http.post(this.pathApi+'/api/category-save', data);
     }
 
     getAllCategory() {
-        return this.http.get('http://localhost:8000/api/get-all-category-by-grid').map((response: Response) => response.json());
+        return this.http.get(this.pathApi+'/api/get-all-category-by-grid').map((response: Response) => response.json());
     }
 
     getCategory(id) {
-        return this.http.post('http://localhost:8000/api/get-category', { id: id }).map((response: Response) => response.json());
+        return this.http.post(this.pathApi+'/api/get-category', { id: id }).map((response: Response) => response.json());
     }
 
     categoryUpdate(data) {
-        return this.http.post('http://localhost:8000/api/category-update', data);
+        return this.http.post(this.pathApi+'/api/category-update', data);
     }
 
     categoryDelete(id) {
-        return this.http.post('http://localhost:8000/api/category-delete', { id: id });
+        return this.http.post(this.pathApi+'/api/category-delete', { id: id });
     }
 
 }
