ed9c23637787462f36e72c4ee6c0e3e3f1002124
Smythy <smythy.costa@hotmail.com>
2018-08-07 14:37:20 -0300

    contrução do metodo login

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,4 +1,27 @@
+import { Injectable } from "@angular/core";
+import { Http, Headers, Response } from '@angular/http';
 
+@Injectable()
 export class AuthenticationService{
     
+    private pathApi: String = 'http://localhost:8000';
+
+    constructor(private http: Http) { }
+
+    login(data) {
+        return this.http.post(this.pathApi+'/api/user-login', data)
+            .map((response: Response) => {
+                
+                // login successful if there's a jwt token in the response
+                let user = response.json();
+                
+                if (user && user.token) {
+                    // store user details and jwt token in local storage to keep user logged in between page refreshes
+                    localStorage.setItem('currentUser', JSON.stringify(user));
+                }
+
+                return user;
+
+            });
+    }
 }
