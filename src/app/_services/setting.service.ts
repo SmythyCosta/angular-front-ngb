93418a430d1d735b4df4b9fdb4c901ef9ed49ea8
Smythy <smythy.costa@hotmail.com>
2018-08-07 14:48:38 -0300

    finalizado o servico SettingService

--- src/app/_services/setting.service.ts
+++ src/app/_services/setting.service.ts
@@ -1,3 +1,20 @@
-export class SettingService{
-    
+import { Injectable } from "@angular/core";
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+
+@Injectable()
+export class SettingService {
+
+    private pathApi: String = 'http://localhost:8000';
+
+    constructor(private http: Http){
+    }
+
+    getSettingData() {
+        return this.http.get(this.pathApi+'/api/get-setting-data').map((response: Response) => response.json());
+    }
+
+    settingUpdate(data){
+    	return this.http.post(this.pathApi+'/api/setting-data-update',data).map((response: Response) => response.json());
+    }
+
 }
