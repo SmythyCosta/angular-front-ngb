fc9c7cc7ea61e2a693f0da06973af34e299df529
Smythy <smythy.costa@hotmail.com>
2018-08-08 11:36:35 -0300

    importes modulos para trabalho com Forms

--- src/app/setting/setting.module.ts
+++ src/app/setting/setting.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule }  from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { SettingFormComponent } from './setting-form/setting-form.component';
 import { SettingRoutes } from './setting.routes';
 import { DirectivasModule } from '../_directives/directives.module';
@@ -7,6 +10,8 @@ import { DirectivasModule } from '../_directives/directives.module';
 @NgModule({
 	imports: [
 		CommonModule,
+		FormsModule,
+		ReactiveFormsModule,
 		DirectivasModule,
 		SettingRoutes
 	],
