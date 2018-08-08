c0c73e158e0a1599cb01d8b500ac2b031b6e17be
Smythy <smythy.costa@hotmail.com>
2018-08-08 09:16:12 -0300

    SettingRoutes

--- src/app/setting/setting.module.ts
+++ src/app/setting/setting.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingFormComponent } from './setting-form/setting-form.component';
 import { SettingListComponent } from './setting-list/setting-list.component';
+import { SettingRoutes } from './setting.routes';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SettingRoutes
   ],
   declarations: [ SettingFormComponent, SettingListComponent],
   exports:[ SettingFormComponent, SettingListComponent]
