62e6141545fe08a83df583d6f011a777d1bf88f8
Smythy <smythy.costa@hotmail.com>
2018-08-08 09:21:31 -0300

    SettingModule refatoração deste modulo

--- src/app/setting/setting.module.ts
+++ src/app/setting/setting.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingFormComponent } from './setting-form/setting-form.component';
-import { SettingListComponent } from './setting-list/setting-list.component';
 import { SettingRoutes } from './setting.routes';
 
 @NgModule({
@@ -9,7 +8,7 @@ import { SettingRoutes } from './setting.routes';
     CommonModule,
     SettingRoutes
   ],
-  declarations: [ SettingFormComponent, SettingListComponent],
-  exports:[ SettingFormComponent, SettingListComponent]
+  declarations: [ SettingFormComponent],
+  exports:[ SettingFormComponent]
 })
 export class SettingModule { }
--- src/app/setting/setting.routes.ts
+++ src/app/setting/setting.routes.ts
@@ -1,13 +1,11 @@
 import { NgModule }                 from '@angular/core';
 import { RouterModule, Routes }     from '@angular/router';
-import { SettingListComponent } from './setting-list/setting-list.component';
 import { SettingFormComponent } from './setting-form/setting-form.component';
 
 
 const settingRoutes: Routes = [
-    { path: 'setting',                  component: SettingListComponent },
-    { path: 'setting/form',             component: SettingFormComponent },
-    { path: 'setting/form/:id',         component: SettingFormComponent }
+    { path: 'setting',                  component: SettingFormComponent },
+    { path: 'setting/form',             component: SettingFormComponent }
 ];
 
 
