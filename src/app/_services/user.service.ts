5ffb76c909562ec43d577592eaea0bf562190e98
Smythy Costa <smythy.costa@hotmail.com>
2018-08-09 21:46:20 -0300

    UserService save

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from "@angular/core";
-import { Http } from "@angular/http";
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
 
 @Injectable()
 export class UserService {
 
     constructor(private http:Http) { }
+
+    save(data){
+        return this.http.post('/api/user-create',data).map((response: Response) => response.json());
+    }
 
 }
