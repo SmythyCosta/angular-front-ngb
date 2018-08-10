c87aa3096056f56d2d63b2d01ce1b4f1335a6211
Smythy Costa <smythy.costa@hotmail.com>
2018-08-09 21:09:45 -0300

    constructor

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -1,11 +1,19 @@
 import { Injectable } from "@angular/core";
-import { CanActivate } from "@angular/router";
+import { CanActivate, Router } from "@angular/router";
+import { AppService } from "../_services/app.service";
+import { AlertService } from "../_services/alert.services";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+
+    constructor(
+        private router: Router,
+        private AppService: AppService,
+        private alertService: AlertService
+    ){}
     
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
         throw new Error("Method not implemented.");
     }
-    
+
 }
