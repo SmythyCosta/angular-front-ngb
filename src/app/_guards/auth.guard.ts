e24a73bd810063d52571970b602b92e03c9ab68b
Smythy Costa <smythy.costa@hotmail.com>
2018-08-09 21:21:03 -0300

    canActivate return false

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -48,11 +48,15 @@ export class AuthGuard implements CanActivate {
             
             // logged in so return true
             return true;
-            
-        }
-    
 
-        return false;
+        }else{
+            
+            // not logged in so redirect to login page with the return url
+            this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
+            return false;
+
+        }
+        
     }
 
 }
