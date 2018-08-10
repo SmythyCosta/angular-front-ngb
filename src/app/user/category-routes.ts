86622fd1b7ac003c1641a45e716b261f64d144f7
Smythy Costa <smythy.costa@hotmail.com>
2018-08-09 21:29:36 -0300

    pasta para construção do modulo user

---
+++ src/app/user/category-routes.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes }     from '@angular/router';
+import { CategoryListComponent }    from './category-list/category-list.component';
+import { CategoryFormComponent }    from './category-form/category-form.component';
+
+
+const categoryRoutes: Routes = [
+    { path: 'category',             component: CategoryListComponent },
+    { path: 'category/form',        component: CategoryFormComponent },
+    { path: 'category/form/:id',    component: CategoryFormComponent }
+];
+
+
+@NgModule({
+    imports: [RouterModule.forChild(categoryRoutes)],
+    exports: [RouterModule]
+})
+export class CategoryRoutes { }
---
+++ src/app/user/category.module.ts
@@ -0,0 +1,20 @@
+import { NgModule }     from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule }  from '@angular/forms';
+
+import { CategoryListComponent }    from './category-list/category-list.component';
+import { CategoryFormComponent }    from './category-form/category-form.component';
+import { CategoryRoutes }           from './category-routes';
+import { CategoryService }          from '../_services/category.service';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        CategoryRoutes
+    ],
+    declarations: [CategoryListComponent, CategoryFormComponent],
+    exports: [CategoryListComponent, CategoryFormComponent],
+    providers:[CategoryService]
+})
+export class CategoryModule { }
