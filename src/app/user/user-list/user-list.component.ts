3078cd0689dae6cc70efa3f88d469319518b8acd
Smythy <smythy.costa@hotmail.com>
2018-08-15 12:35:03 -0300

    ajustes para utilizar o modal

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit, Input, Output, EventEmitter, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ElementRef, TemplateRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
+
+//
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { UserService } from '../../_services/user.service';
 import { AlertService } from '../../_services/alert.services';
 import { AppService } from '../../_services/app.service';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 class User {
     id: number;
@@ -22,8 +25,8 @@ class User {
 })
 export class UserListComponent implements OnInit {
 
-    public titlePage:String = "User";
-    public countJson:number;
+    public titlePage: String = "User";
+    public countJson: number;
 
     @Input() allowMultiple: boolean;
     @Input() fileType: string;
@@ -68,11 +71,16 @@ export class UserListComponent implements OnInit {
         image: ''
     };
 
+    /** variaveis relativa ao Modal */
+    public modalRef: BsModalRef; //Modal
+    public info: String;         //Modal
+    public modelDelete: String;  //Modal
 
     constructor(
         public router: Router,
         private dataService: UserService,
         private alertService: AlertService,
+        private modalService: BsModalService,
         private elementRef: ElementRef
     ) { }
 
@@ -91,6 +99,16 @@ export class UserListComponent implements OnInit {
         });
 
         this.allUser();
+    }
+
+    /**
+     * @param template 
+     * Modal Bootstrap
+    */
+    public openModal(template: TemplateRef<any>, info, id) {
+        this.modalRef = this.modalService.show(template); //
+        this.info = info;
+        this.modelDelete = id;
     }
 
     /**
@@ -115,14 +133,14 @@ export class UserListComponent implements OnInit {
 
         // alert('Live Demo Button Not Working');
         //if (confirm('Are you sure?')) {
-            this.dataService.userDelete(id)
-                .pipe().subscribe(data => {
-                    this.dtTrigger = new Subject(); //  DataTable
-                    this.allUser();
-                    this.alertService.success('User Delete successful', true);
-                }, error => {
-                    this.alertService.error(error);
-                });
+        this.dataService.userDelete(id)
+            .pipe().subscribe(data => {
+                this.dtTrigger = new Subject(); //  DataTable
+                this.allUser();
+                this.alertService.success('User Delete successful', true);
+            }, error => {
+                this.alertService.error(error);
+            });
         //}
 
     }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,11 +1,17 @@
+// ==================== default angular ====================
 import { NgModule }     from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule }  from '@angular/forms';
+
+// ==================== libs ====================
+import { DataTablesModule } from 'angular-datatables';
+
+// ==================== integrations ====================
 import { UserRoutesRoutes } from './user-routes';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserFormComponent } from './user-form/user-form.component';
 import { UserService } from '../_services/user.service';
-import { DataTablesModule } from 'angular-datatables';
+
 
 
 @NgModule({
