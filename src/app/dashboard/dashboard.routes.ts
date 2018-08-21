5026c5fb242b8e42baa7dd45daa3047f4408de08
Smythy Costa <smythy.costa@hotmail.com>
2018-08-20 22:14:41 -0300

    DashBoardRoutes

--- src/app/dashboard/dashboard.routes.ts
+++ src/app/dashboard/dashboard.routes.ts
@@ -1,10 +1,16 @@
-import { NgModule }             from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent }   from './dashboard.component';
+import { DashboardComponent } from './dashboard.component';
 
 
 const dashboardRoutes: Routes = [
-    { path: 'dashboard',        component: DashboardComponent },
+    {
+        path: '',
+        component: DashboardComponent,
+        data: {
+            title: 'Dashboard'
+        }
+    }
 ];
 
 
