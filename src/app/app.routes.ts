39b66eb627e8dd663374dc82b26449e3823a1449
Smythy Costa <smythy.costa@hotmail.com>
2018-08-20 21:24:39 -0300

    AppRouter refatorado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { FullLayoutComponent } from './layout/full-layout.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './_guards/index';
 
-const appRoutes: Routes = [
+
+export const appRoutes: Routes = [
+
+    {   
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+    },
+    {
+        path: '',
+        component: FullLayoutComponent,
+        data: {
+            title: 'Home'
+        },
+        children:[
+            { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule', canActivate: [AuthGuard] },
+            { path: 'category', loadChildren: './category/category.module#CategoryModule', canActivate: [AuthGuard] },
+            { path: 'subcategory', loadChildren: './sub-category/sub-category.module#SubCategoryModule', canActivate: [AuthGuard] },
+            { path: 'product', loadChildren: './product/product.module#ProductModule', canActivate: [AuthGuard] },
+            { path: 'user', loadChildren: './user/user.module#UserModule', canActivate: [AuthGuard] }
+        ]
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+        data: {
+            title: 'login'
+        }
+    }
     
-    { path: 'login',            redirectTo: '/login', pathMatch: 'full' },
-    { path: '**',               redirectTo: '/login' }
-
-    /**
-    { path: 'dashboard',        redirectTo: '/dashboard' },
-    { path: 'category',         redirectTo: '/category' },
-    { path: 'sub-category',     redirectTo: '/sub-category' },
-     */
-    
-
 ];
-
 
 @NgModule({
     imports: [RouterModule.forRoot(appRoutes)],
