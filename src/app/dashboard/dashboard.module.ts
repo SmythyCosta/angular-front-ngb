480bf8582a4c4700ba758bf813e202ee63c99595
Smythy Costa <smythy.costa@hotmail.com>
2018-08-20 22:08:48 -0300

    DashboardModel

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,5 +1,6 @@
 import { NgModule }             from '@angular/core';
 import { CommonModule }         from '@angular/common';
+
 import { DashboardComponent }   from './dashboard.component';
 import { DasboardRoutes }       from './dashboard.routes';
 
@@ -10,6 +11,6 @@ import { DasboardRoutes }       from './dashboard.routes';
         DasboardRoutes
     ],
     declarations: [DashboardComponent],
-    exports: [DashboardComponent]
+    providers: [DashboardService]
 })
 export class DashboardModule { }
