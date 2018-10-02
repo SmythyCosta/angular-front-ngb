e35c865c236e7aa709de9fcc9841cfda243711f9
Smythy <smythy.costa@hotmail.com>
2018-10-02 16:31:01 -0300

    sales module

--- src/app/sales/sales.module.ts
+++ src/app/sales/sales.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule }  from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
-
 //import { ProductListComponent } from './product-list/product-list.component';
 //import { ProductFormComponent } from './product-form/product-form.component';
 //import { ProductService } from '../_services/product.service';
+import { SalesComponent } from './sales/sales.component';
 import { SalesRoutes } from './sales.routes';
 import { DirectivasModule } from '../_directives/directives.module';
 
@@ -20,6 +20,7 @@ import { DirectivasModule } from '../_directives/directives.module';
     ],
     declarations: [
         //ProductListComponent, ProductFormComponent
+        SalesComponent
     ],
     exports: [ 
         //ProductListComponent, ProductFormComponent 
