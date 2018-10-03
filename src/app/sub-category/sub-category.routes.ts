79b455bca7b0fcbf640a0b9c089c7bb2bd0c6ca3
Smythy <smythy.costa@hotmail.com>
2018-10-03 09:02:53 -0300

    refatoracao - subCategory routes

--- src/app/sub-category/sub-category.routes.ts
+++ src/app/sub-category/sub-category.routes.ts
@@ -1,15 +1,13 @@
 import { NgModule }                 from '@angular/core';
 import { RouterModule, Routes }     from '@angular/router';
-import { SubCategoryComponent }     from './sub-category.component';
-import { SubCategoryFormComponent } from './sub-category-form.component';
-
+import { SubCategoryComponent }     from './list/sub-category.component';
+import { SubCategoryFormComponent } from './form/sub-category-form.component';
 
 const subCategoryRoutes: Routes = [
     { path: '', component: SubCategoryComponent, data: {title: 'SubCategory List'} },
     { path: 'form', component: SubCategoryFormComponent, data: {title: 'SubCategory Manage'} },
     { path: 'form/:id', component: SubCategoryFormComponent, data: {title: 'SubCategory Manage'} }
 ];
-
 
 @NgModule({
     imports: [RouterModule.forChild(subCategoryRoutes)],
