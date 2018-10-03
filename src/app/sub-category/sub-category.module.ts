95cbe21777e450cd614dff1a7644afa88137fcda
Smythy <smythy.costa@hotmail.com>
2018-10-03 09:02:02 -0300

    refatoracao - subCategory module

--- src/app/sub-category/sub-category.module.ts
+++ src/app/sub-category/sub-category.module.ts
@@ -2,8 +2,8 @@ import { NgModule }     from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule }  from '@angular/forms';
 
-import { SubCategoryComponent } from './sub-category.component';
-import { SubCategoryFormComponent } from './sub-category-form.component';
+import { SubCategoryComponent } from './list/sub-category.component';
+import { SubCategoryFormComponent } from './form/sub-category-form.component';
 import { SubCategoryService } from '../_services/sub-category.service';
 import { SubCategoryRoutes } from './sub-category.routes';
 import { DataTablesModule } from 'angular-datatables';
