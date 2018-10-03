f878ec1038c77ad28461280692bbc70ab841cc8f
Smythy <smythy.costa@hotmail.com>
2018-10-03 17:47:23 -0300

    refatoracao - setting, finalizado

--- src/app/setting/setting-form/setting-form.component.ts
+++ src/app/setting/setting-form/setting-form.component.ts
@@ -13,8 +13,6 @@ import { AlertService } from '../../_services/alert.service';
 })
 export class SettingFormComponent implements OnInit {
 
-    settingAddForm: FormGroup;
-
     setting = {
         id: '',
         company_name: '',
@@ -32,30 +30,18 @@ export class SettingFormComponent implements OnInit {
     @Output() onSelection = new EventEmitter<FileList>();
     DisplayedText: string = "";
     fileList: any;
+    settingAddForm: FormGroup;
 
-
-    constructor(
-        public router: Router,
-        private http: Http,
-        private dataService: SettingService,
-        private alertService: AlertService
-    ) { }
-
+    constructor(public router: Router,
+                private http: Http,
+                private dataService: SettingService,
+                private alertService: AlertService
+                ) {}
 
     ngOnInit() {
-
-        this.settingAddForm = new FormGroup({
-            company_name: new FormControl("", Validators.compose([Validators.required])),
-            address: new FormControl("", Validators.compose([Validators.required])),
-            phone: new FormControl("", Validators.compose([Validators.required])),
-            email: new FormControl("", Validators.compose([Validators.required])),
-            currency: new FormControl("", Validators.compose([Validators.required]))
-        });
-
+        this.formGroupValidation();
         this.settingData();
-    
     }
-
 
     /**
      * Chama a api do BackEnd
@@ -73,10 +59,8 @@ export class SettingFormComponent implements OnInit {
                     currency: data.setting.currency,
                     image: data.setting.image
                 }
-
             });
     }
-
 
     /**
      * Trigger 
@@ -87,74 +71,56 @@ export class SettingFormComponent implements OnInit {
         this.insertAction();
     }
 
-
     /**
      * @param event
      * Trata o evento de Upload 
      */
     fileChange(event: any) {
-
         this.fileList = event.target.files;
         let hasFile = this.fileList && this.fileList.length > 0;
-
         if (hasFile) {
-
             var extension = this.fileList[0].name.substring(this.fileList[0].name.lastIndexOf('.'));
-
             // Only process image files.
             var validFileType = ".jpg , .png , .bmp";
-
             if (validFileType.toLowerCase().indexOf(extension) < 0) {
                 alert("please select valid file type. The supported file types are .jpg , .png , .bmp");
                 this.fileList = null;
                 this.DisplayedText = "";
                 return false;
             }
-
             if (this.fileList[0].size > 65535) {
                 alert(`File size is more than 65 Kb`);
                 this.fileList = null;
                 this.DisplayedText = "";
                 return false;
             }
-
             let multipleFile = this.fileList.length > 1;
-
             if (multipleFile) {
                 this.DisplayedText = this.fileList.length + ' file(s) has been selected';
-            }
-
-            else {
+            }else {
                 let file: File = this.fileList[0];
                 this.DisplayedText = file.name;
             }
-
             this.onSelection.emit(this.fileList);
-
         }
-
     }
-
 
     /**
      * Atualiza Setings
      * Informaçoes da Companhia
      */
     insertAction() {
-
         let formData: FormData = new FormData();
-
         if (this.fileList != undefined) {
             let file: File = this.fileList[0];
             formData.append('file', file, file.name);
         }
-
         // ================ formData ================
-        formData.append('company_name', this.setting.company_name);
-        formData.append('phone', this.setting.phone);
-        formData.append('email', this.setting.email);
-        formData.append('address', this.setting.address);
-        formData.append('currency', this.setting.currency);
+        formData.append('company_name', this.settingAddForm.value.company_name);
+        formData.append('phone', this.settingAddForm.value.phone);
+        formData.append('email', this.settingAddForm.value.email);
+        formData.append('address', this.settingAddForm.value.address);
+        formData.append('currency', this.settingAddForm.value.currency);
 
         this.dataService.settingUpdate(formData)
             .subscribe(data => {
@@ -163,8 +129,20 @@ export class SettingFormComponent implements OnInit {
             }, error => {
                 this.alertService.error(error);
             });
-
     }
 
+    /**
+     * FormGroup
+     * https://angular.io/guide/reactive-forms
+    */
+    formGroupValidation(): any {
+        this.settingAddForm = new FormGroup({
+            company_name: new FormControl("", Validators.compose([Validators.required])),
+            address: new FormControl("", Validators.compose([Validators.required])),
+            phone: new FormControl("", Validators.compose([Validators.required])),
+            email: new FormControl("", Validators.compose([Validators.required])),
+            currency: new FormControl("", Validators.compose([Validators.required]))
+        });
+    }
 
 }
