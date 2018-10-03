b3a61a89b2e3e162fb1e4f20a309ba3febb16753
Smythy <smythy.costa@hotmail.com>
2018-10-03 12:13:22 -0300

    refatoracao - product list ts allProduct metodo

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -28,53 +28,20 @@ export class ProductListComponent implements OnInit {
     
 
     ngOnInit() {
-
-
-        // ============================== Begin Testing ============================
-        /*
-        * Test Call API
-        */
-       /**
-        let stream = this.http.get('http://127.0.0.1:8000/api/all-product');
-        stream.map(res => res.json())
-        .subscribe(data => {
-            this.productList = data.product;
-
-            console.log('API Call Values => ');
-            console.log(this.productList);
-        
-        }, error => console.log(error));
-        */
-        //
-        // ============================== End Testing ============================
-
         this.allProduct();
-
     }
 
-
+    /**
+     * Carrega todos dados de Entity
+     * @returns Objto Json
+     */
     allProduct() {
         this.dataService.getAllProduct()
             .subscribe(data => {
                 this.productList = data.product;
+                this.countJson = this.lengthJson(this.productList);
             });
     }
-
-    /**
-    delete(id) {
-        this.dataService.productDelete(id)
-            .subscribe(data => {
-                if ( data.status == 200 ) {
-                    alert('Product Delete successful');
-                    this.allProduct();
-                } else {
-                    alert('Product Assigned Product');
-                }
-            }, error => {
-                alert(error);
-            });
-    }
-     */
 
     /**
      * @param obj 
