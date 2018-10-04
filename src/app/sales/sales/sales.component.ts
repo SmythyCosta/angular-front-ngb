90bbb289a771da8b33c17e0d931f251a07a7a870
Smythy Costa <smythy.costa@hotmail.com>
2018-10-04 19:38:37 -0300

    trabalhando com datas

--- src/app/sales/sales/sales.component.ts
+++ src/app/sales/sales/sales.component.ts
@@ -1,33 +1,60 @@
-import { Component, OnInit,Input,OnChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import { Http } from '@angular/http';
-import { AlertService, AppService } from '../../_services/index';
-
-
+import { AlertService, AppService, SalesService } from '../../_services/index';
+import * as moment from 'moment';
 
 @Component({
-  selector: 'app-sales',
-  templateUrl: './sales.component.html'
+    selector: 'app-sales',
+    templateUrl: './sales.component.html'
 })
 export class SalesComponent implements OnInit {
 
-    constructor(public router: Router,
-                private http:Http,
-                private alertService: AlertService,
-                private dataService:SalesService,
-                private AppService:AppService,
-                ){}
-	
-	ngOnInit() {
-		
-    }
-  
+    sales:any;
+    categoryList: Object[] = [];
 
-  category(){
-		this.dataService.getAllCategory()
-			.subscribe(data => {
-				this.categoryList = data.cat;
-			});
-	}
-	
+    constructor(public router: Router,
+        private http: Http,
+        private alertService: AlertService,
+        private dataService: SalesService,
+        private AppService: AppService,
+    ) { }
+
+    ngOnInit() {
+
+        let d = new Date();
+        let randomnumber = moment(d).format('YYMMDDHHmmss');
+        let formatDate = moment(d).format('YYYY-MM-DD');
+        let dateArry = formatDate.split('-');
+        let setDate = { year: Number(dateArry[0]), month: Number(dateArry[1]), day: Number(dateArry[2]) };
+
+        this.sales = {
+			invoiceCode:randomnumber,
+			customer: "",
+			date: setDate,
+			allCategory: "",
+			subCategory: "",
+			allProduct: "",
+			subTotal: "",
+			discount: "0",
+			vat: "0",
+			grandTotal:"",
+			receivedAmount:"",
+			due:"",
+			paymentType:"0",
+			products: [
+				// {amount: 111.11, date: "Jan 1, 2016", final: false},
+				// {amount: 222.22, date: "Jan 2, 2016", final: true}
+				]
+		}
+    }
+
+
+    category() {
+        this.dataService.getAllCategory()
+            .subscribe(data => {
+                this.categoryList = data.cat;
+            });
+    }
+
 }
