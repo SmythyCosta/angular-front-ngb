f81fc84e4d37577c553c104c3a058372b580551e
Smythy Costa <smythy.costa@hotmail.com>
2018-10-04 19:26:25 -0300

    dependencia e metodo category

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,3 @@
 export const appConfig = {
-    apiUrl: 'http://127.0.0.1:8080'
+    apiUrl: 'http://127.0.0.1:8000'
 };
--- src/app/sales/sales/sales.component.ts
+++ src/app/sales/sales/sales.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit,Input,OnChanges } from '@angular/core';
+import { Router } from '@angular/router';
+import { Http } from '@angular/http';
+import { AlertService, AppService } from '../../_services/index';
+
 
 
 @Component({
@@ -6,9 +10,24 @@ import { Component, OnInit,Input,OnChanges } from '@angular/core';
   templateUrl: './sales.component.html'
 })
 export class SalesComponent implements OnInit {
+
+    constructor(public router: Router,
+                private http:Http,
+                private alertService: AlertService,
+                private dataService:SalesService,
+                private AppService:AppService,
+                ){}
 	
 	ngOnInit() {
 		
+  }
+  
+
+  category(){
+		this.dataService.getAllCategory()
+			.subscribe(data => {
+				this.categoryList = data.cat;
+			});
 	}
 	
 }
